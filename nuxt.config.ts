a301b794cfd37b1b3d15db72be3fe300177d975b
Sam Brown <sam.brown@akerolabs.com>
2025-10-23 15:53:36 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  ssr: true,
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@nuxt/content'],
+  css: ['~/assets/css/main.css']
+})
